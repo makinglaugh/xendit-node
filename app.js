60e529e8065664400709e549d8cedbbb0ebe1592
Xendit trial <xendit@GetCrafts-MacBook-Pro-7.local>
2020-02-25 13:07:08 +0700

    fix database connection when GET

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ var MongoClient = require('mongodb').MongoClient;
 var os = require('os');
 
 app.get('/ostype', function(req, res) {
+ insertDate();
+ res.send("OS Type: "+os.type()+"\nOS Platform: "+os.platform()+"\nOS Release "+os.release())
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+function insertDate() {
  MongoClient.connect("mongodb://xendit-mongo:27017/xenditdb", function (err, db) {
      if(err) throw err;
  var dbo = db.db("xenditdb");
  dbo.collection('logging').insert({date: new Date(Date.now()).toISOString()}, function(err, r) {
 
  console.log("query executed");
+   });
  });
+} 
 
- });
- res.send("OS Type: "+os.type()+"\nOS Platform: "+os.platform()+"\nOS Release "+os.release())
-});
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
